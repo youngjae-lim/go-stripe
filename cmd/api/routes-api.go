24c9936226f6b5bff32db303bdf36f926f82f032
Youngjae Lim <limyoungjae@gmail.com>
2021-09-13 19:48:25 -0400

    Added a backend authentication that has a drawback we need to resolve later

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -24,6 +24,7 @@ func (app *application) routes() http.Handler {
 	mux.Post("/api/subscribe", app.SubscribeToPlan)
 
 	mux.Post("/api/authenticate", app.CreateAuthToken)
+	mux.Post("/api/is-authenticated", app.CheckAuthentication)
 
 	return mux
 }
