181038d807bef8114fab2a26ca85f76d5f7ad6ae
Youngjae Lim <limyoungjae@gmail.com>
2021-09-13 23:53:37 -0400

    added a middleware to protect specific routes on the backend

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -26,5 +26,9 @@ func (app *application) routes() http.Handler {
 	mux.Post("/api/authenticate", app.CreateAuthToken)
 	mux.Post("/api/is-authenticated", app.CheckAuthentication)
 
+	mux.Route("/api/admin", func(mux chi.Router) {
+		mux.Use(app.Auth)
+	})
+
 	return mux
 }
