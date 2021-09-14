e27320efb2c6cebd6799407880b85370b5dad894
Youngjae Lim <limyoungjae@gmail.com>
2021-09-13 23:34:24 -0400

    Added checking for expiry when validating a token on the backend

--- internal/models/tokens.go
+++ internal/models/tokens.go
@@ -56,14 +56,15 @@ func (m *DBModel) InsertToken(t *Token, u User) error {
 	}
 
 	stmt = `insert into tokens
-						(user_id, name, email, token_hash, created_at, updated_at)
-					 values (?, ?, ?, ?, ?, ?)
+						(user_id, name, email, token_hash, expiry, created_at, updated_at)
+					 values (?, ?, ?, ?, ?, ?, ?)
 	`
 	_, err = m.DB.ExecContext(ctx, stmt,
 		u.ID,
 		u.LastName,
 		u.Email,
 		t.Hash,
+		t.Expiry,
 		time.Now(),
 		time.Now(),
 	)
@@ -86,12 +87,12 @@ func (m *DBModel) GetUserForToken(token string) (*User, error) {
 					u.id, u.first_name, u.last_name, u.email
 				from
 					users u inner join tokens t on (u.id = t.user_id)
-				where t.token_hash = ?
+				where t.token_hash = ? and t.expiry > ?
 	`
 
 	var user User
 
-	err := m.DB.QueryRowContext(ctx, query, tokenHash[:]).Scan(
+	err := m.DB.QueryRowContext(ctx, query, tokenHash[:], time.Now()).Scan(
 		&user.ID,
 		&user.FirstName,
 		&user.LastName,
