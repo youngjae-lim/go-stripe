f47e1e4f3f14eb9e78d0244fb396548c754b3fb9
Youngjae Lim <limyoungjae@gmail.com>
2021-09-21 20:40:44 -0400

    set up a simple microservice to generate a invoice

---
+++ cmd/micro/invoice/invoice.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+	}
+	frontend_url string
+}
+
+type application struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+	version  string
+}
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	app.infoLog.Printf("Starting invoice microservice on port %d", app.config.port)
+
+	return srv.ListenAndServe()
+}
+
+func main() {
+	var cfg config
+
+	// set flags for config
+	flag.IntVar(&cfg.port, "port", 5000, "Server port to listen on")
+	flag.StringVar(&cfg.smtp.host, "smtphost", "smtp.mailtrap.io", "smtp host")
+	flag.StringVar(&cfg.smtp.username, "smtpusername", "b407d9befe3e65", "smtp username")
+	flag.IntVar(&cfg.smtp.port, "smtpport", 587, "smtp port")
+	flag.StringVar(&cfg.frontend_url, "frontend_url", "http://localhost:4000", "url to front end")
+
+	// parse the flags
+	flag.Parse()
+
+	// mailtrap.io password
+	cfg.smtp.password = os.Getenv("MAILTRAP_PASSWORD")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+		version:  version,
+	}
+
+	err := app.serve()
+	if err != nil {
+		app.errorLog.Println(err)
+		log.Fatal(err)
+	}
+}
